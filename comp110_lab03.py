576634c2fae3b03d0ac2750ff653f3437c956d4c
Sat Garcia <sat@sandiego.edu>
2020-02-11 11:19:46 -0800

    updated to new sound module

--- comp110_lab03.py
+++ comp110_lab03.py
@@ -12,11 +12,14 @@ sound.wait_until_played()  # waits until love_sound is done playing
 
 # change the volume of the love sound
 for i in range(len(love_sound)):
-    sample = love_sound.get_sample(i)
-    new_left_val = sample.get_left() * 2
-    new_right_val = sample.get_right() * 2
-    sample.set_left(new_left_val)
-    sample.set_right(new_right_val)
+    sample = love_sound[i]
+
+    # change the left channel
+    new_left_val = sample.left * 2
+    sample.left = new_left_val
+
+    # change the right channel (in only one line of code!)
+    sample.right = sample.right * 2
 
 love_sound.play()
 sound.wait_until_played()  # waits until love_sound is done playing
